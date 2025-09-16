a7c1d3a70b7afc4945470309ed7e44f4f6158986
Thanhdo2110 <nguyendo.51947@gmail.com>
2025-09-16 13:06:00 +0700

    security p2

---
+++ src/main/java/com/example/possystem/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.possystem.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET ="ashudsaudjsndjsadwqd,uhasdhdhisaihdsaihdsa,asidhsaidhsaidhsad,ijsadhsadihsa";
+    public static final String JWT_HEADER ="Authorization";
+}
--- src/main/java/com/example/possystem/configuration/JwtValidator.java
+++ src/main/java/com/example/possystem/configuration/JwtValidator.java
@@ -1,12 +1,25 @@
 package com.example.possystem.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.FilterChain;
+import org.apache.coyote.BadRequestException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.http.SecurityHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.security.Key;
+import java.util.List;
 
 public class JwtValidator extends OncePerRequestFilter {
     @Override
@@ -14,6 +27,33 @@ public class JwtValidator extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        String jwt = request.getHeader("Authorization");
+        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+
+        if(jwt != null){
+            jwt = jwt.substring(7);
+
+            try{
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes());
+                Claims claims = Jwts.parser()
+                        .verifyWith(key)
+                        .build()
+                        .parseSignedClaims(jwt)
+                        .getPayload();
+
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String .valueOf(claims.get("authorities"));
+
+                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+
+                Authentication auth = new UsernamePasswordAuthenticationToken(email , null , auths);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
+
+            }
+            catch (Exception e){
+                throw new BadRequestException("Invalid JWT...");
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
